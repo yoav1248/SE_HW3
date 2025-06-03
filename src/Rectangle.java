74a5cb3aa696e70cc24d38941d512fe0de87d444
ofek7 <ofek75309@gmail.com>
2025-06-03 20:41:54 +0300

    Recangle.toString

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -25,4 +25,13 @@ public class Rectangle extends Shape {
     public int getHeight() {
         return height;
     }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (int rows = 0; rows < height; rows++) {
+            str = str.concat(" * ".repeat(rows) + "\n");
+        }
+        return str;
+    }
 }
